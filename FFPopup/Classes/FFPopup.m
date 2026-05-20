d33641d0dc5dc78b913bccc5a996d342106f5bbb
Ana <40877166+MagicalAna@users.noreply.github.com>
2026-05-20 16:24:15 +0800

    commit

--- FFPopup/Classes/FFPopup.m
+++ FFPopup/Classes/FFPopup.m
@@ -583,7 +583,7 @@ const FFPopupLayout FFPopupLayout_Center = { FFPopupHorizontalLayout_Center, FFP
                     case FFPopupDismissType_SlideOutToBottom: {
                         CGRect finalFrame = strongSelf.containerView.frame;
                         finalFrame.origin.y = CGRectGetHeight(strongSelf.bounds);
-                        [UIView animateWithDuration:dismissOutDuration delay:0.0 options:kAnimationOptionCurve animations:^{
+                        [UIView animateWithDuration:dismissOutDuration delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
                             strongSelf.containerView.frame = finalFrame;
                         } completion:completionBlock];
                     }   break;
